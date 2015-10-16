91e759554fc180a2fc60e4e2c629378dfc9a71eb
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2015-10-16 10:20:39 -0400

    high performance git branch in prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
+function _promtp_git_branch
+  echo (git rev-parse --abbrev-ref HEAD 2> /dev/null | awk '{print " (" $1 ")"}')
+end
+
 function fish_prompt
 	
   # set -l last_status $status
@@ -28,11 +32,11 @@ function fish_prompt
   set -l brwhite (set_color -o white)
  
   # Configure __fish_git_prompt
-  set -g __fish_git_prompt_showdirtystate false
-  set -g __fish_git_prompt_showuntrackedfiles false
-  set -g __fish_git_prompt_showstashstate false
-  set -g __fish_git_prompt_color purple
-  set -g __fish_git_prompt_color_flags purple
+  # set -g __fish_git_prompt_showdirtystate false
+  # set -g __fish_git_prompt_showuntrackedfiles false
+  # set -g __fish_git_prompt_showstashstate false
+  # set -g __fish_git_prompt_color purple
+  # set -g __fish_git_prompt_color_flags purple
  
   # Color prompt char red for non-zero exit status
   # set -l pcolor $gray
@@ -46,7 +50,8 @@ function fish_prompt
   end
 
   echo ''
-  echo -s $green$USER$white@$cyan(hostname)$normal ' ' $yellow(prompt_pwd)$normal (__fish_git_prompt)
+  echo -s $green$USER$white@$cyan(hostname)$normal ' ' $yellow(prompt_pwd)$normal $purple(_promtp_git_branch)$normal
   echo -s -n $white $_venv
   echo $red'❯'$yellow'❯'$green'❯ '$normal
+
 end
