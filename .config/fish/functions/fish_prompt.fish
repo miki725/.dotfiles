3c88df7ec7e4a0c612071aa74719452a6d4b15c2
Miroslav Shubernetskiy <miroslav@miki725.com>
2016-11-12 21:07:46 -0500

    added elapsed time to fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,6 +2,39 @@ function _promtp_git_branch
   echo (git rev-parse --abbrev-ref HEAD 2> /dev/null | awk '{print " (" $1 ")"}')
 end
 
+function _elapsed_time
+  set -l seconds $argv[1]
+
+  if not test -n "$seconds"
+    echo "Must provide time"
+    return 1
+  end
+
+  if test (echo "$seconds>3600" | bc) -eq 1
+    set -l hours (echo "$seconds/3600" | bc)
+    set -l minutes (echo "($seconds-(3600*($seconds/3600)))/60" | bc)
+    set -l seconds (echo "$seconds -(60*($seconds/60))" | bc)
+
+    if test $hours -gt 1
+      set hours_text "hours"
+    else
+      set hours_text "hour"
+    end
+
+    printf '%d %s %d minutes %f seconds' $hours $hours_text $minutes $seconds
+
+  else if test (echo "$seconds>60" | bc) -eq 1
+    set -l minutes (echo "$seconds/60" | bc)
+    set -l seconds (echo "$seconds -(60*($seconds/60))" | bc)
+    printf '%d minutes %f seconds' $minutes $seconds
+
+  else
+    echo "$seconds sec"
+
+  end
+
+end
+
 function fish_prompt
 	
   # set -l last_status $status
@@ -49,6 +82,7 @@ function fish_prompt
   	set _venv "("(basename "$VIRTUAL_ENV" '')") "
   end
 
+  set -q CMD_DURATION; and echo "# elapsed time" (_elapsed_time (echo "$CMD_DURATION/1000" | bc -l))
   echo ''
   echo -s $green$USER$white@$cyan(hostname)$normal ' ' $yellow(prompt_pwd)$normal $purple(_promtp_git_branch)$normal
   echo -s -n $white $_venv
