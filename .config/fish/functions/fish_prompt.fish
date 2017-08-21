1c235b23072a31b39e840c9f0185c2bc085eb751
Miroslav Shubernetskiy <miroslav@miki725.com>
2017-08-20 22:26:45 -0400

    elapsed time in red on non-zero exit code

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -36,9 +36,9 @@ function _elapsed_time
 end
 
 function fish_prompt
-	
-  # set -l last_status $status
- 
+
+  set -l last_status $status
+
   # Just calculate these once, to save a few cycles when displaying the prompt
   # if not set -q __fish_prompt_hostname
   #   set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
@@ -51,7 +51,7 @@ function fish_prompt
   #   set -g __fish_prompt_char '>'
   #   end
   # end
- 
+
   # Setup colors
   set -l normal (set_color normal)
   set -l red (set_color red)
@@ -63,26 +63,31 @@ function fish_prompt
   set -l gray (set_color -o cyan)
   set -l purple (set_color -o purple)
   set -l brwhite (set_color -o white)
- 
+
   # Configure __fish_git_prompt
   # set -g __fish_git_prompt_showdirtystate false
   # set -g __fish_git_prompt_showuntrackedfiles false
   # set -g __fish_git_prompt_showstashstate false
   # set -g __fish_git_prompt_color purple
   # set -g __fish_git_prompt_color_flags purple
- 
+
   # Color prompt char red for non-zero exit status
   # set -l pcolor $gray
   # if test $last_status -ne 0
   #   set pcolor $red
   # end
- 
+
   set -l _venv ''
   if set -q VIRTUAL_ENV
   	set _venv "("(basename "$VIRTUAL_ENV" '')") "
   end
 
-  set -q CMD_DURATION; and echo "# elapsed time" (_elapsed_time (echo "$CMD_DURATION/1000" | bc -l))
+  set -l elapsed_color $normal
+  if test $last_status -ne 0
+    set elapsed_color $red
+  end
+
+  set -q CMD_DURATION; and echo $elapsed_color"# elapsed time" (_elapsed_time (echo "$CMD_DURATION/1000" | bc -l))$normal
   echo ''
   echo -s $green$USER$white@$cyan(hostname)$normal ' ' $yellow(prompt_pwd)$normal $purple(_promtp_git_branch)$normal
   echo -s -n $white $_venv
