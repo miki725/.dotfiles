1c7931af46923e9a11b99e0b5ec050b14527a4c7
Miroslav Shubernetskiy <miroslav@miki725.com>
2017-08-29 16:01:46 -0400

    gracefully setting env vars if deps are not present

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,28 @@
-eval (python3 -m virtualfish auto_activation compat_aliases projects)
-eval (direnv hook fish)
+if test (python3 -m virtualfish 2> /dev/null)
+    eval (python3 -m virtualfish auto_activation compat_aliases projects)
+end
+if test (which direnv)
+    eval (direnv hook fish)
+end
 
 set -gx ANDROID_HOME /usr/local/opt/android-sdk
 
-set -gx PATH \
+set -l path \
     /usr/local/bin \
     /usr/local/sbin \
-    /usr/local/opt/coreutils/libexec/gnubin \
-    $PATH
+    /usr/local/opt/coreutils/libexec/gnubin
 
-set -gx MANPATH \
-    /usr/local/opt/coreutils/libexec/gnuman \
-    $MANPATH
+for i in $path[-1..1]
+    if test -d $i
+        set -gx PATH $i $PATH
+    end
+end
+
+set -gx manpath \
+    /usr/local/opt/coreutils/libexec/gnuman
+
+for i in $manpath[-1..1]
+    if test -d $i
+        set -gx MANPATH $i $MANPATH
+    end
+end
