12ffa5de7e9346dc2e22098651e6f09d9688486e
Miroslav Shubernetskiy <miroslav@miki725.com>
2018-12-25 16:30:30 -0500

    added gpg config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 set -gx ANDROID_HOME /usr/local/opt/android-sdk
 set -gx LANG en_US.utf-8
 set -gx GPG_TTY (tty)
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+set -gx SSH_AGENT_PID ""
+gpg-connect-agent updatestartuptty /bye > /dev/null
 
 set -l path \
     $HOME/.local/bin \
