5430ceb36a53e0fc65098717f9aa4dc0d9f03d06
Miroslav Shubernetskiy <miroslav@miki725.com>
2019-09-01 10:35:01 -0400

    faster fish init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,6 @@ set SPACEFISH_PROMPT_ORDER \
     git \
     venv \
     node \
-    docker \
     aws  \
     exec_time \
     line_sep \
@@ -18,6 +17,8 @@ set -gx EDITOR vim
 set -gx GPG_TTY (tty)
 set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -gx SSH_AGENT_PID ""
+set -gx N_PREFIX $HOME/.n
+
 gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
 
 # modify macOS system SSH_AUTH_SOCK if it does not match
@@ -33,48 +34,58 @@ if which nvim > /dev/null 2>&1
     alias vim=nvim
 end
 
-set -l path \
-    $HOME/.local/bin \
-    $HOME/.cargo/bin \
-    $HOME/.yarn/bin \
-    $HOME/go/bin \
-    $HOME/Library/Python/3.7/bin \
-    $HOME/.config/yarn/global/node_modules/.bin \
-    /usr/local/bin \
-    /usr/local/sbin \
-    $HOME/.pyenv/shims \
-    /usr/local/opt/python/libexec/bin \
-    /usr/local/opt/coreutils/libexec/gnubin \
-    /usr/local/opt/curl/bin \
-    /usr/local/opt/openssl@1.1/bin \
-    /usr/local/opt/openssl/bin \
-    /usr/local/opt/gettext/bin \
-    /usr/local/opt/findutils/libexec/gnubin \
-    /usr/local/opt/gnu-sed/libexec/gnubin \
-    /usr/local/opt/gnu-tar/libexec/gnubin \
-    /usr/local/opt/gnu-which/libexec/gnubin \
-    /usr/local/opt/grep/libexec/gnubin \
-    /usr/local/opt/ruby/bin \
-    /Library/TeX/texbin
+if not contains $HOME/.fish-path-hook $PATH
+    set -gx PATH $PATH $HOME/.fish-path-hook
 
-for i in $path[-1..1]
-    # remove duplicates
-    # cant simply use contains condition since order could be changed
-    # better to remove and then add back to PATH
-    while contains $i $PATH
-        set -e PATH[(contains -i $i $PATH)]
-    end
-    if test -d $i
-        set -gx PATH $i $PATH
-    end
-end
+    set path \
+        $HOME/.local/bin \
+        $HOME/.cargo/bin \
+        $HOME/.yarn/bin \
+        $HOME/.n/bin \
+        $HOME/go/bin \
+        $HOME/Library/Python/3.7/bin \
+        $HOME/.config/yarn/global/node_modules/.bin \
+        /usr/local/bin \
+        /usr/local/sbin \
+        /usr/local/opt/python/libexec/bin \
+        /usr/local/opt/coreutils/libexec/gnubin \
+        /usr/local/opt/curl/bin \
+        /usr/local/opt/openssl@1.1/bin \
+        /usr/local/opt/openssl/bin \
+        /usr/local/opt/gettext/bin \
+        /usr/local/opt/findutils/libexec/gnubin \
+        /usr/local/opt/gnu-sed/libexec/gnubin \
+        /usr/local/opt/gnu-tar/libexec/gnubin \
+        /usr/local/opt/gnu-which/libexec/gnubin \
+        /usr/local/opt/grep/libexec/gnubin \
+        /usr/local/opt/ruby/bin \
+        /Library/TeX/texbin
 
-# sometimes within subshell when VIRTUAL_ENV is already set
-# above PATH adjustements will put VIRTUAL_ENV not on top of PATH
-# hence ignoring most of PATH order
-if set -q VIRTUAL_ENV; and contains $VIRTUAL_ENV/bin $PATH
-	set -e PATH[(contains -i $VIRTUAL_ENV/bin $PATH)]
-	set -gx PATH $VIRTUAL_ENV/bin $PATH
+    if test -d $HOME/.pyenv/versions
+        for i in (find ~/.pyenv/versions/ -depth 2 -name bin -type d)
+            set -a path $i
+        end
+    end
+
+    for i in $path[-1..1]
+        # remove duplicates
+        # cant simply use contains condition since order could be changed
+        # better to remove and then add back to PATH
+        while contains $i $PATH
+            set -e PATH[(contains -i $i $PATH)]
+        end
+        if test -d $i
+            set -gx PATH $i $PATH
+        end
+    end
+
+    # sometimes within subshell when VIRTUAL_ENV is already set
+    # above PATH adjustements will put VIRTUAL_ENV not on top of PATH
+    # hence ignoring most of PATH order
+    if set -q VIRTUAL_ENV; and contains $VIRTUAL_ENV/bin $PATH
+        set -e PATH[(contains -i $VIRTUAL_ENV/bin $PATH)]
+        set -gx PATH $VIRTUAL_ENV/bin $PATH
+    end
 end
 
 if ! test -e $HOME/.manpath
@@ -104,10 +115,15 @@ for i in $compilepath[-1..1]
     end
 end
 
-if which python3 > /dev/null 2>&1
+if ! test -e $HOME/.virtualfish.fish
+        and which python3 > /dev/null 2>&1
         and python3 -m virtualfish > /dev/null 2>&1
-    eval (python3 -m virtualfish auto_activation compat_aliases projects)
+    python3 -m virtualfish auto_activation compat_aliases projects > $HOME/.virtualfish.fish
 end
+if test -e $HOME/.virtualfish.fish
+    source $HOME/.virtualfish.fish
+end
+
 if which direnv > /dev/null 2>&1
     eval (direnv hook fish)
 end
