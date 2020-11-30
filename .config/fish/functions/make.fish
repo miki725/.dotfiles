eb86c0c58a2c6de182e0e58b66b2c6d5bb30c630
Miroslav Shubernetskiy <miroslav@miki725.com>
2020-11-30 00:53:13 -0500

    better pipx upgrade for diff python and brew fixes

--- .config/fish/functions/make.fish
+++ .config/fish/functions/make.fish
@@ -1,6 +1,8 @@
 function make
     if test "$PWD" = "$HOME"
-        command make -f .Makefile $argv
+            and test -f $HOME/.Makefile
+        set -l OPTIONS -f $HOME/.Makefile
+        command env OPTIONS="$OPTIONS" make $OPTIONS $argv
     else
         command make $argv
     end
