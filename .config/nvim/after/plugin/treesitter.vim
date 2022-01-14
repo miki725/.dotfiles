91356b157d94f029bb470e77f5b0c2d5aff7716e
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-01-13 21:24:23 -0500

    adding treesitter context and playground for debuggin

--- .config/nvim/after/plugin/treesitter.vim
+++
@@ -1,7 +0,0 @@
-" folding needs to switching to nvim_treesitter
-" only after treesitter loads
-" otherwise either the syntax is not highlighted
-" until :e or folding does not work
-" https://www.reddit.com/r/neovim/comments/njew3z/treesitter_code_folding/
-set foldmethod=expr
-set foldexpr=nvim_treesitter#foldexpr()
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,29 +2,80 @@ return function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
+		requires = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-refactor",
+		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
+				-- built-in modules
 				highlight = {
 					enable = true,
 					use_languagetree = true,
 					disable = {},
 				},
 				indent = {
-					enable = false,
+					enable = true,
 					disable = {},
 				},
-				refactor = {
-					highlight_definitions = {
-						enable = true,
-					},
+				autopairs = {
+					enable = true,
 				},
 				rainbow = {
 					enable = true,
 					extended_mode = true,
 				},
+
+				-- additinal plugins
+				refactor = {
+					highlight_definitions = {
+						enable = true,
+					},
+					highlight_current_scope = {
+						enable = false,
+					},
+				},
+				textobjects = {
+					enable = true,
+					select = {
+						lookahead = true,
+						enable = true,
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
 				ensure_installed = "maintained",
+			})
+			vim.opt.foldmethod = "expr"
+			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		end,
+	})
+	use({
+		"romgrk/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				patterns = {
+					default = {
+						"class",
+						"function",
+						"method",
+					},
+					terraform = {
+						"block",
+					},
+				},
 			})
 		end,
 	})
-	use({ "nvim-treesitter/nvim-treesitter-refactor" })
+	use({
+		"nvim-treesitter/playground",
+		cmd = {
+			"TSPlaygroundToggle",
+		},
+	})
 end
