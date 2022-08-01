afb62a70ed8b789b5f1609a4907e9bdb1d1e496b
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-08-01 12:27:07 -0400

    updaing keyring before update

--- .config/fish/functions/recreate-pacman-keys.fish
+++ .config/fish/functions/recreate-pacman-keys.fish
@@ -1,4 +1,5 @@
 function recreate-pacman-keys --description 'recreate all pacman keys from scratch'
+    pacman -Syu archlinux-keyring gnupg
     rm -fr /etc/pacman.d/gnupg
     pacman-key --init
     pacman-key --populate archlinux
