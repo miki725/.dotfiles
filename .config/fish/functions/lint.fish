c2e4cf3368fb3e82af3eb00e5bb712e3cc3b5fbf
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-08-30 09:26:27 -0400

    better lint helper

--- .config/fish/functions/lint.fish
+++ .config/fish/functions/lint.fish
@@ -1,8 +1,28 @@
 function lint --description 'run pre-commit lint on all changes files'
-    argparse -i a/all -- $argv
+    argparse -i a/all d/diff -- $argv
+    set -l diff ""
+    if test -n "$_flag_diff"
+        set diff --show-diff-on-failure
+    end
     if test -z "$_flag_all"
-        pre-commit run $argv --files (git status --short | awk '{print $2}') --show-diff-on-failure
+        set -l files
+        set -l args
+        for i in $argv
+            if test -e $i
+                set files $files $i
+            else
+                set args $args $i
+            end
+        end
+        if test -z "$files"
+            set files (git status --short | awk '{print $2}')
+        end
+        if test -n "$files"
+            pre-commit run --files $files $diff $args
+        else
+            pre-commit run $diff $args
+        end
     else
-        pre-commit run $argv --all-files
+        pre-commit run --all-files $diff $argv
     end
 end
