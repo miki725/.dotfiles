13b13c32f2f431ded2afd1b6e765e1770a3b16be
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-12-26 09:00:00 -0500

    adding description to aws_creds

--- .config/fish/functions/aws_creds.fish
+++ .config/fish/functions/aws_creds.fish
@@ -1,4 +1,4 @@
-function aws_creds
+function aws_creds --description="export aws creds from current profile as env vars"
     set -gx AWS_ACCESS_KEY_ID (aws configure get aws_access_key_id)
     set -gx AWS_SECRET_ACCESS_KEY (aws configure get aws_secret_access_key)
 end
