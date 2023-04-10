6b44cb3e18e8576a02011039fda41e984eda3b8f
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-04-10 11:24:21 -0400

    moving sshfix prompt fix to conf.d

---
+++ .config/fish/conf.d/sshfix.fish
@@ -0,0 +1,12 @@
+function __sshfix --on-event fish_prompt
+    if not set -q SSH_AUTH_SOCK
+        return
+    end
+    if not set -q TMUX
+        return
+    end
+    if not test -S $SSH_AUTH_SOCK
+        echo SSH auth socket is out of date. updating >/dev/stderr
+        sshfix
+    end
+end
--- .config/fish/functions/sshfix.fish
+++ .config/fish/functions/sshfix.fish
@@ -1,16 +1,3 @@
 function sshfix
     bass eval (tmux show-env -s)
 end
-
-function __sshfix --on-event fish_prompt
-    if not set -q SSH_AUTH_SOCK
-        return
-    end
-    if not set -q TMUX
-        return
-    end
-    if not test -S $SSH_AUTH_SOCK
-        echo SSH auth socket is out of date. updating >/dev/stderr
-        sshfix
-    end
-end
