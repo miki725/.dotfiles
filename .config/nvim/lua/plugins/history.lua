5c2fd7cb32be193efdf9e8a81aae9971cc72cc17
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-09-28 09:01:15 -0400

    adding nvim-fundo

--- .config/nvim/lua/plugins/history.lua
+++ .config/nvim/lua/plugins/history.lua
@@ -1,11 +1,21 @@
 return {
     {
+        "kevinhwang91/nvim-fundo",
+        dependencies = { "kevinhwang91/promise-async" },
+        build = function()
+            require("fundo").install()
+        end,
+        opts = {
+            archives_dir = vim.fn.stdpath("cache") .. "/fundo",
+        },
+    },
+    {
         "dawsers/telescope-file-history.nvim",
         event = { "BufWritePost" },
         main = "file_history",
         dev = true,
         opts = {
-            backup_dir = "~/.local/state/nvim/file-history-git",
+            backup_dir = vim.fn.stdpath("cache") .. "/file-history-git",
         },
     },
 }
