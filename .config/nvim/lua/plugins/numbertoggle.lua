9541da6df960a89cf1ad9e693b702d12decae67c
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-10-12 22:10:03 -0400

    adding toggle for relative numbers within the buffer

--- .config/nvim/lua/plugins/numbertoggle.lua
+++ .config/nvim/lua/plugins/numbertoggle.lua
@@ -1,4 +1,11 @@
 return {
     -- relative line numbers
-    { "jeffkreeftmeijer/vim-numbertoggle" },
+    {
+        "jeffkreeftmeijer/vim-numbertoggle",
+        init = function()
+            vim.keymap.set("n", "<leader>tn", function()
+                vim.o.relativenumber = not vim.o.relativenumber
+            end, { desc = "Toggle Relative Numbers [NumberToggle]" })
+        end,
+    },
 }
