4f2f705a8dcf1cb711753d54a4c55a2a4992a6b7
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-10-15 20:50:17 -0400

    switching to git linker for file links from fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,19 +2,10 @@ return {
     -- adds git commands such as :Gdiffssplit for 3-way-merge
     {
         "tpope/vim-fugitive",
-        dependencies = {
-            "tpope/vim-rhubarb", -- required for gbrowse
-        },
         cmd = {
-            "GBrowse",
             "Gdiffsplit",
             "Git",
             "Gvdiffsplit",
         },
-        init = function()
-            vim.cmd([[
-				command! -nargs=* Browse
-			]])
-        end,
     },
 }
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -3,4 +3,30 @@ return {
     { "rhysd/committia.vim" },
     -- for interactive rebase
     { "hotwatermorning/auto-git-diff" },
+    {
+        "ruifm/gitlinker.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            {
+                "<leader>gy",
+                function()
+                    require("gitlinker").get_buf_range_url(string.lower(vim.fn.mode()), {
+                        add_current_line_on_normal_mode = true,
+                    })
+                end,
+                mode = { "n", "v" },
+                desc = "Copy line/selection git link [GitLinker]",
+            },
+            {
+                "<leader>gY",
+                function()
+                    require("gitlinker").get_buf_range_url(string.lower(vim.fn.mode()), {
+                        add_current_line_on_normal_mode = false,
+                    })
+                end,
+                mode = { "n", "v" },
+                desc = "Copy file git link [GitLinker]",
+            },
+        },
+    },
 }
