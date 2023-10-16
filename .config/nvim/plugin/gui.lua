a0af35ac3f3e0db14bcccef4d21b47d4b7cdf57d
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-10-15 21:11:20 -0400

    default PWD in gui vim to $HOME

--- .config/nvim/plugin/gui.lua
+++ .config/nvim/plugin/gui.lua
@@ -1,3 +1,9 @@
 -- for GUI apps like neovide
 vim.o.guifont = "Hack Nerd Font"
 vim.g.neovide_cursor_animation_length = 0
+
+local pwd = vim.api.nvim_exec("pwd", true)
+local is_gui = vim.fn.has("gui_running") > 0
+if pwd == "/" and is_gui then
+    vim.cmd.cd(vim.fn.expand("$HOME"))
+end
