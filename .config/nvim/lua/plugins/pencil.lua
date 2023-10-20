067f66deb1ebca6ebff26d09b6b5697c1e2d5ff3
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-10-20 11:40:10 -0400

    removing conceal from pencil

--- .config/nvim/lua/plugins/pencil.lua
+++ .config/nvim/lua/plugins/pencil.lua
@@ -3,9 +3,15 @@ return {
         "preservim/vim-pencil",
         cmd = {
             "Pencil",
+            "PencilToggle",
+            "PencilSort",
+            "PencilHard",
             "TogglePencil",
             "SoftPencil",
             "HardPendil",
         },
+        init = function()
+            vim.g["pencil#conceallevel"] = 0
+        end,
     },
 }
