c22e392248e70ed4b343ceeebfd77fdbffcaf877
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-11-15 09:56:37 -0500

    markdown preview is conditional

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -2,6 +2,9 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         build = "cd app && yarn install",
+        cond = function()
+            return vim.fn.executable("lua-language-server") > 0
+        end,
         ft = { "markdown" },
         init = function()
             vim.g.mkdp_open_to_the_world = 1
