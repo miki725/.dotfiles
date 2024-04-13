d24e2a0f53a6602c054f6b8c7029a7f6812a626d
Miroslav Shubernetskiy <miroslav@miki725.com>
2024-04-13 11:28:11 -0400

    updates

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -3,7 +3,7 @@ return {
         "iamcco/markdown-preview.nvim",
         build = "cd app && yarn install",
         cond = function()
-            return vim.fn.executable("lua-language-server") > 0
+            return vim.fn.executable("yarn") > 0
         end,
         ft = { "markdown" },
         init = function()
