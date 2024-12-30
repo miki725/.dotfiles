47e3cb9b2cdd8b918716e1d2c58c0fc585bede3c
Miroslav Shubernetskiy <miroslav@miki725.com>
2024-12-30 09:07:00 -0500

    switching to device auth for aws sso wrapper

--- .config/fish/functions/aws_sso.fish
+++ .config/fish/functions/aws_sso.fish
@@ -1,14 +1,17 @@
 function aws_sso
-    aws sso login | python -c '
-import sys, re
+    aws sso login --use-device-code | python -c '
+import sys, re, socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+ip = ips[0]
 url = ""
 code = ""
 for l in sys.stdin:
-    print(l, end="")
     if l.startswith("https:"):
+        l = l.replace("127.0.0.1", ip)
         url = l.strip()
     elif re.search(r"\w{4}-\w{4}", l):
         code = l.strip()
+    print(l, end="")
     if url and code:
         print()
         print(f"{url}?user_code={code}")
