84b86db10368fb29622cbb9024803d9ce694f345
Miroslav Shubernetskiy <miroslav@miki725.com>
2025-02-20 10:07:50 -0500

    loading quickfix-reflector as top-level plugin

--- .config/nvim/lua/plugins/ripgrep.lua
+++ .config/nvim/lua/plugins/ripgrep.lua
@@ -1,9 +1,10 @@
 return {
     {
+        "stefandtw/quickfix-reflector.vim",
+        ft = { "qf" },
+    },
+    {
         "miki725/vim-ripgrep",
-        dependencies = {
-            "stefandtw/quickfix-reflector.vim",
-        },
         init = function()
             vim.g.rg_command = "rg --vimgrep -S"
             vim.g.rg_highlight = 1
