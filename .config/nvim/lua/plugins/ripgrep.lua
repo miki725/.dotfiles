6d0fadd16821ab797ba1a0ead3b72975759f2948
Miroslav Shubernetskiy <miroslav@miki725.com>
2025-03-04 16:06:30 -0500

    switch to quicker

--- .config/nvim/lua/plugins/ripgrep.lua
+++ .config/nvim/lua/plugins/ripgrep.lua
@@ -1,14 +1,45 @@
 return {
     {
-        "stefandtw/quickfix-reflector.vim",
+        "vim-scripts/QFEnter",
         ft = { "qf" },
+        init = function()
+            vim.g.qfenter_vopen_map = { "<C-v>" }
+            vim.g.qfenter_hopen_map = { "<C-CR>", "<C-s>", "<C-x>" }
+            vim.g.qfenter_topen_map = { "<C-t>" }
+        end,
+    },
+    {
+        "stevearc/quicker.nvim",
+        ft = { "qf" },
+        opts = {
+            keys = {
+                {
+                    ">",
+                    function()
+                        require("quicker").expand({
+                            before = 1,
+                            after = 1,
+                            add_to_existing = true,
+                        })
+                    end,
+                    desc = "Expand quickfix content",
+                },
+                {
+                    "<",
+                    function()
+                        require("quicker").collapse()
+                    end,
+                    desc = "Collapse quickfix content",
+                },
+            },
+        },
     },
     {
         "miki725/vim-ripgrep",
         init = function()
             vim.g.rg_command = "rg --vimgrep -S"
             vim.g.rg_highlight = 1
-            vim.g.rg_apply_mappings = 1
+            vim.g.rg_apply_mappings = 0
         end,
         cmd = "Rg",
     },
