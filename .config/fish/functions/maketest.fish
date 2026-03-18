fa737be01a2db331de10c11ed5552e16bb078eef
Miroslav Shubernetskiy <miroslav@miki725.com>
2026-03-18 10:14:53 -0400

    use ssh for github git

--- .config/fish/functions/maketest.fish
+++ .config/fish/functions/maketest.fish
@@ -1,4 +1,11 @@
 function maketest
+    argparse \
+        --ignore-unknown \
+        't/target=&' \
+        -- $argv
+    if test -z "$_flag_target"
+        set _flag_target tests
+    end
     reset
-    make tests args="$argv"
+    make $_flag_target args="$argv"
 end
