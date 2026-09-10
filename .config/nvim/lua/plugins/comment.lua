5098f115a46eda6f05af6af5b07af59be611c23e
Miroslav Shubernetskiy <miroslav@miki725.com>
2026-09-10 10:14:35 -0400

    remove comment.nvim. its built into nvim now

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,42 +1,6 @@
 local telescope_utils = require("utils.telescope")
 
 return {
-    {
-        "numToStr/Comment.nvim",
-        keys = {
-            { "gc", "gc", desc = "Comment Lines [Comment]" },
-            { "gb", "gb", desc = "Comment Block [Comment]" },
-            { "gc", "gc", desc = "Comment Lines [Comment]", mode = "v" },
-            { "gb", "gb", desc = "Comment Block [Comment]", mode = "v" },
-        },
-        config = function()
-            require("Comment").setup({
-                padding = true,
-                sticky = true,
-                ignore = "^$",
-                toggler = {
-                    line = "gcc",
-                    block = "gbc",
-                },
-                opleader = {
-                    line = "gc",
-                    block = "gb",
-                },
-                extra = {
-                    above = "gcO",
-                    below = "gco",
-                    eol = "gcA",
-                },
-                mappings = {
-                    basic = true,
-                    extra = true,
-                    extended = false,
-                },
-                pre_hook = nil,
-                post_hook = nil,
-            })
-        end,
-    },
     telescope_utils.register({
         "folke/todo-comments.nvim",
         cond = function()
